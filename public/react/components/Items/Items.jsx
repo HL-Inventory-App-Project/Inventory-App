c4022f0151dcd80c6e20dd68f2578265a50b7726
laraibxsunny <laraibsunny373@yahoo.co.uk>
2024-11-18 13:53:33 +0000

    bread

--- public/react/components/Items/Items.jsx
+++ public/react/components/Items/Items.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from "./ItemsStyles.module.css";
 import ItemsCard from './ItemsCard';
 import bread from "../../../assets/bread.png";
+import bread2 from "../../../assets/bread2.png"
+import bread3 from "../../../assets/bread3.png"
 
 function Items() {
   return ( <>
@@ -9,7 +11,7 @@ function Items() {
     <section id="items">
       <div className={styles.itemsContainer}>
       <ItemsCard
-      link="https://www.youtube.com/"
+      link="https://www.youtube.com/watch?v=J1DAmmROUX8"
       src={bread}
       name="Item 1"
       description="Piece of bread"
@@ -17,20 +19,20 @@ function Items() {
       category="bread"
       />
       <ItemsCard
-      link="https://www.youtube.com/"
-      src={bread}
-      name="Item 1"
-      description="Piece of bread"
+      link="https://www.youtube.com/watch?v=J1DAmmROUX8"
+      src={bread2}
+      name="Item 2"
+      description="Piece of bread2"
       price="£4.20"
-      category="bread"
+      category="bread2"
       />
       <ItemsCard
-      link="https://www.youtube.com/"
-      src={bread}
-      name="Item 1"
-      description="Piece of bread"
+      link="https://www.youtube.com/watch?v=J1DAmmROUX8"
+      src={bread3}
+      name="Item 3"
+      description="Piece of bread3"
       price="£4.20"
-      category="bread"
+      category="bread3"
       />
       </div>
     </section>
