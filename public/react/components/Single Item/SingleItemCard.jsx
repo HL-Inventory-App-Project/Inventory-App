ec6aacec9aff5bbc1765b98252ef468b2d7be750
SamAld06 <samgaldred@gmail.com>
2024-11-19 11:42:32 +0000

    styled css for single item view (made it sparkly!)

--- public/react/components/Single Item/SingleItemCard.jsx
+++ public/react/components/Single Item/SingleItemCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from "./ItemStyles.module.css";
 
 function ItemCard({ link, name, src, description, price, category }) {
 
@@ -19,18 +20,20 @@ function ItemCard({ link, name, src, description, price, category }) {
 	  }
 
   return (
-    <div>
-    	<img src={src}></img>
-		<div>
-    		<h3>{name}</h3>
+	<a className={styles.item}>
+		<div className={styles.itemText}>
+			<h3>{name}</h3>
 			<h4>£{price}</h4>
-    		<p>{description}</p>
+			<p>{description}</p>
 			<h4>Category:</h4>
-    		<p>{category}</p>
-			<button onClick={deleteItem}>Delete Item</button>
-			<button onClick={goHome}>Back to Home</button>
+			<p>{category}</p>
+			<div>
+				<button onClick={deleteItem}><span>Delete Item</span></button>
+				<button onClick={goHome}><span>Back to Home</span></button>
+			</div>
 		</div>
-    </div>
+		<img src={src}></img>
+	</a>
   )
 }
 
