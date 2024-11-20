ed0d0c76ade0f237d63879799e0307d45986ce88
cspark <git@cspark.dev>
2024-11-20 16:56:29 +0000

    Hardcode apiURL for this demo/as a workaround

--- public/react/api.js
+++ public/react/api.js
@@ -3,7 +3,9 @@ let apiURL;
 if (process.env.NODE_ENV === "development") {
   apiURL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
 } else {
-  apiURL = `https://${window.location.hostname}-api`;
+  // Production
+  // Hardcoding the apiURL for this example. Ideally you'd have the api on something like https://api.hl-inventory-app-api.com in a real world scenario so the strings would be easier to concatenate
+  apiURL = `https://hl-inventory-app-api.onrender.com`;
 }
 
 export default apiURL;
