514734cfe3f99d643c42e8c091886619c72bad4e
cspark <git@cspark.dev>
2024-11-20 16:57:35 +0000

    Fix comment

--- public/react/api.js
+++ public/react/api.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === "development") {
   apiURL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
 } else {
   // Production
-  // Hardcoding the apiURL for this example. Ideally you'd have the api on something like https://api.hl-inventory-app-api.com in a real world scenario so the strings would be easier to concatenate
+  // Hardcoding the apiURL for this example. Ideally you'd have the api on something like https://api.hl-inventory-app.com in a real world scenario so the strings would be easier to concatenate
   apiURL = `https://hl-inventory-app-api.onrender.com`;
 }
 
